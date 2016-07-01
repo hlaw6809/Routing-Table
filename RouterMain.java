ac945cef140c9b1ff69ca74c18ccfda6a8de3bd5
avalvillar <avalvillar@gmail.com>
2016-06-30 17:12:01 -0700

    Updated Files.

---
+++ RouterMain.java
@@ -0,0 +1,83 @@
+package router;
+
+import java.util.Random;
+
+public class RouterMain {
+    private static final int ADD_WAIT = 5000;           //Time to wait in ms
+    private static final int INITIAL_TABLE_SIZE = 0;   //Initial size of routing table
+    private static final int MAX_MASK_SIZE = 24;        //Maximum size of address mask
+    private static final int MIN_MASK_SIZE = 12;        //Minimum size of address mask
+    private static final int NUM_PORTS = 16;            //Number of ports on the router
+    private static final int ADD_LOOPS = 10;            //Number of times to add a route to the table
+
+    private static RoutingTable table;
+
+
+    /**
+     * Makes a route with a random destination address and port number.
+     * @return random route
+     */
+    private static Route makeRandomRoute() {
+        Random rand = new Random();
+        int maskSize = rand.nextInt(MAX_MASK_SIZE - MIN_MASK_SIZE) + MIN_MASK_SIZE; //Generate random mask size between 12 and 24
+        String dest = Integer.toString(rand.nextInt(255)) + '.' + rand.nextInt(255) + '.' + rand.nextInt(255) + '.' + rand.nextInt(255) + '/' + maskSize; //Generate random IP address
+        int port = rand.nextInt(NUM_PORTS);
+        return new Route(dest, port, true);
+    }
+
+
+    /**
+     * Initializes and populated the routing table
+     */
+    private static void initializeTable() {
+        //Create Table
+        table = new RoutingTable();
+
+        //Add number of random routes to table
+        for (int i = 0; i < INITIAL_TABLE_SIZE; i++) {
+            table.addRoute(makeRandomRoute());
+        }
+
+        //Print table
+        System.out.println("Initial Table:");
+        System.out.println(table.toString());
+    }
+
+
+    public static void main(String[] args) {
+        initializeTable();
+        for (int i = 0; i < ADD_LOOPS; i++) {
+            //Wait to add
+            try {
+                Thread.sleep(ADD_WAIT);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            //Add new Route
+            Route randRoute = makeRandomRoute();
+            table.addRoute(randRoute);
+
+            //Print information
+            System.out.println("Route Added: " + randRoute.toString());
+            System.out.println();
+            System.out.println(table.toString());
+            
+            if (i % 3 == 0) {
+            	for (Route r : table.getTable()){
+            		if (!r.isActiveFlag()) {
+            			table.removeRoute(r.getSeqNumber());
+            		}
+            	}
+            }
+        }
+
+        //Test best route search for an IP
+        table.addRoute(new Route("255.255.255.0/16", 1, true));
+        String testIP = "255.255.255.111";
+        System.out.println("Searching for best route for " + testIP);
+        Route best = table.findBestRoute(testIP);
+        System.out.println("Best Route for " + testIP + ": " + best.toString());
+    }
+
+}
---
+++ RoutingTable.java
@@ -0,0 +1,145 @@
+package router;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Created by hlawr_000 on 6/29/2016.
+ */
+public class RoutingTable {
+
+    private static List<Route> table;
+
+    public RoutingTable() {
+        table = new ArrayList<Route>();
+    }
+
+
+    /**
+     * Adds a route to the table
+     * @param toAdd Route to add to the table
+     */
+    public void addRoute(Route toAdd) {
+        table.add(toAdd);
+        update();
+    }
+    
+    public List<Route> getTable() {
+    	return table;
+    }
+    
+    public Route getRoute(int routeSeq) {
+    	return table.get(routeSeq);
+    }
+    
+    public void removeRoute(int routeSeq) {
+    	Route temp = getRoute(routeSeq);
+    	long temp2 = new Date().getTime();
+    	if (temp2 - temp.getTimeStamp().getTime() > 4000) {		
+    		table.remove(routeSeq);
+    	}	
+    }
+
+    /**
+     * This method is used to simulate an update of a route on a nearby router.
+     * @param portNum   Port number the updated router is on
+     * @param destAddr  Destination Address that was updated
+     * @param cost      New cost of the route. -1 will set the route inactive
+     */
+    public void updateRoute(int portNum, String destAddr, int cost) {
+        //Find route with destination address
+        boolean routeFound = false;
+        for (int i = 0; i < table.size(); i++) {
+            Route route = table.get(i);
+            if (route.getPortNumber() == portNum && route.getDestAddress().equals(destAddr)) {
+                if (cost == -1) {
+                    route.setActiveFlag(false);
+                } else {
+                    route.setDestCost(cost);
+                    route.setActiveFlag(true);
+                }
+                route.setTimeStamp(new Date());
+            }
+        }
+
+        //If no route found create new one
+        if (!routeFound && cost != -1) {
+            table.add(new Route(destAddr, portNum, true));
+        }
+
+        //Update route table now that it has new information
+        update();
+    }
+
+
+    /**
+     * Updates entries on the table
+     */
+    private void update() {
+        aggregate();
+        //Other updates
+    }
+
+
+    /**
+     * Reduces entries on table by combining entries on the same port using CIDR aggregation
+     */
+    private void aggregate() {
+        //To be implemented
+    }
+
+
+    public Route findBestRoute(String address) {
+        Route bestRoute = null;
+        for (Route route : table) {
+            if (route.isActiveFlag() && compareAddress(address, route.getDestAddress())) {
+                System.out.println("    Route Found: " + route.toString());
+                if (bestRoute == null || getMaskSize(route.getDestAddress()) < getMaskSize(bestRoute.getDestAddress())) {
+                    bestRoute = route;
+                }
+            }
+        }
+        return bestRoute;
+    }
+
+
+    private boolean compareAddress(String destAddress, String routeAddress) {
+        int maskSize = getMaskSize(routeAddress);
+        int routeIP = getIPInt(routeAddress);
+        int destIP = getIPInt(destAddress);
+        int bitMask = 0x80000000;
+        for (int i = 0; i < maskSize; i++) {
+            if ((destIP & bitMask) != (routeIP & bitMask)) {
+                return false;
+            }
+            bitMask = bitMask >> 1;
+        }
+        return true;
+    }
+
+
+    private int getIPInt(String address) {
+        String[] addrInfo = address.split("/");
+        String[] ipStrings = addrInfo[0].split("\\.", 4);
+        int ip =  (Integer.parseInt(ipStrings[0]) << 24)
+                + (Integer.parseInt(ipStrings[1]) << 16)
+                + (Integer.parseInt(ipStrings[2]) << 8)
+                + Integer.parseInt(ipStrings[3]);
+        return ip;
+    }
+
+    private int getMaskSize(String address) {
+        String[] addrInfo = address.split("/");
+        return Integer.parseInt(addrInfo[1]);
+    }
+
+    @Override
+    public String toString() {
+        String result = "Routing Table: \n";
+        for (int index = 0; index < table.size(); index++) {
+            result += "    Table Entry " + index + ": " + table.get(index).toString() + "\n";
+        }
+        return result;
+    }
+}
